9686bd325291a6e706e15ea2f8d6076595d878b0
opituf <121101929+opituf@users.noreply.github.com>
2023-02-07 22:04:40 +0900

    1

--- Assets/Scripts/Enemy/CombatState.cs
+++ Assets/Scripts/Enemy/CombatState.cs
@@ -13,26 +13,30 @@ public class CombatState : MonoBehaviour
 
     private void Update()
     {
+        LookToTarget(_player.transform.position);
+
         if (CheckShootingPossibility())
         {
+            _agent.enabled = false;
             _weapon.TryShoot(_player.transform.position - transform.position);
-            LookToTarget(_player.transform.position);
         }
         else
         {
-            FollowTarget(_player.transform);
-            LookToTarget(_player.transform.position);
+            _agent.enabled = true;
+            _agent.SetDestination(_player.transform.position);
         }
     }
 
     private bool CheckShootingPossibility()
     {
         Ray2D ray = new Ray2D(transform.position, _sprite.transform.right);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 10);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 12);
 
-        // Debug.DrawRay(ray.origin, ray.direction * _weapon.Data.FireRange, Color.red);
+        Debug.DrawRay(ray.origin, ray.direction * 12, Color.red);
+        var result = hit.collider != null && hit.collider.TryGetComponent(out Player player);
+        print(result);
 
-        return (hit.collider != null && hit.collider.TryGetComponent(out Player player));
+        return result;
     }
 
     private void LookToTarget(Vector3 target)
@@ -40,10 +44,5 @@ public class CombatState : MonoBehaviour
         Vector3 lookDirection = target - transform.position;
         float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
         _sprite.transform.rotation = Quaternion.Euler(0, 0, lookAngle);
-    }
-
-    private void FollowTarget(Transform target)
-    {
-        _agent.SetDestination(target.position);
     }
 }
