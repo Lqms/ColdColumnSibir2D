88ce62eac03985e11ffd5fd4c216ed8bc1930f03
opituf <121101929+opituf@users.noreply.github.com>
2023-02-07 22:10:11 +0900

    1

--- Assets/Scripts/Enemy/CombatState.cs
+++ Assets/Scripts/Enemy/CombatState.cs
@@ -17,12 +17,11 @@ public class CombatState : MonoBehaviour
 
         if (CheckShootingPossibility())
         {
-            _agent.enabled = false;
+            _agent.SetDestination(transform.position);
             _weapon.TryShoot(_player.transform.position - transform.position);
         }
         else
         {
-            _agent.enabled = true;
             _agent.SetDestination(_player.transform.position);
         }
     }
@@ -32,11 +31,13 @@ public class CombatState : MonoBehaviour
         Ray2D ray = new Ray2D(transform.position, _sprite.transform.right);
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 12);
 
+        /*
         Debug.DrawRay(ray.origin, ray.direction * 12, Color.red);
         var result = hit.collider != null && hit.collider.TryGetComponent(out Player player);
         print(result);
+        */
 
-        return result;
+        return hit.collider != null && hit.collider.TryGetComponent(out Player player); ;
     }
 
     private void LookToTarget(Vector3 target)
