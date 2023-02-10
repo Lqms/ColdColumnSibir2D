59eb08c52366e22c4a548b312d1fb49ef4c33052
Luqmus <rjcnz1007@yandex.ru>
2023-02-10 10:11:46 +0400

    1

--- Assets/Scripts/Enemy/StateMachine/IdleState.cs
+++ Assets/Scripts/Enemy/StateMachine/IdleState.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class IdleState : State
 {
+    [SerializeField] private float _minTimeBetweenTurn = 4;
+    [SerializeField] private float _maxTimeBetweenTurn = 8;
 
+    protected override void Start()
+    {
+        base.Start();
+
+        StartCoroutine(Idling());
+    }
+
+    private IEnumerator Idling()
+    {
+        while (true)
+        {
+            TurnToTarget(Vector2.right * Random.Range(-10, 10));
+            yield return new WaitForSeconds(Random.Range(_minTimeBetweenTurn, _maxTimeBetweenTurn));
+        }
+    }
 }
