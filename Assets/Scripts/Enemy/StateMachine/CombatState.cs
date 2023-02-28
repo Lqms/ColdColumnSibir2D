f24e1c8ad9a8d7ded3f5230853ae669668a30c6b
opituf <121101929+opituf@users.noreply.github.com>
2023-02-28 18:30:57 +0900

    Update CombatState.cs

--- Assets/Scripts/Enemy/StateMachine/CombatState.cs
+++ Assets/Scripts/Enemy/StateMachine/CombatState.cs
@@ -8,8 +8,6 @@ public class CombatState : State
 {
     [SerializeField] private Weapon _weapon;
 
-    private float _rangeOffset = 0.5f;
-
     private void Update()
     {
         TurnToTarget(Player.transform.position);
@@ -30,12 +28,15 @@ public class CombatState : State
         Vector3 rayOffset = new Vector3(0.05f, 0, 0);
         float rangeOffset = 0.5f;
 
+        Ray2D ray = new Ray2D(_weapon.transform.position - rayOffset, _weapon.transform.right);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, _weapon.Data.FireRange - rangeOffset);
+
+        return hit.collider != null && hit.collider.TryGetComponent(out Player player);
+
+        /*
         Ray2D ray = new Ray2D(_weapon.transform.position, _weapon.transform.right);
-        var hits = Physics2D.BoxCastAll(ray.origin, new Vector2(0.1f, 0.1f), 90, ray.direction, _weapon.Data.FireRange - rangeOffset);
-
-        // Ray2D ray = new Ray2D(_weapon.transform.position - rayOffset, _weapon.transform.right);
-        // RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, _weapon.Data.FireRange - rangeOffset);
-
+        var hits = Physics2D.BoxCastAll(ray.origin, new Vector2(0.1f, 0.1f), 90, ray.direction, _weapon.Data.FireRange - rangeOffset); 
+         
         var results = "";
 
         foreach (var hit in hits)
@@ -44,5 +45,6 @@ public class CombatState : State
         print(results);
 
         return hits.Length == 1 && hits[0].collider.TryGetComponent(out Player player);
+        */
     }
 }
