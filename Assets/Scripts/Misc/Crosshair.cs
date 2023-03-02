27555ff2fb52b71533803e3f8fdca3e119fdfc3f
opituf <121101929+opituf@users.noreply.github.com>
2023-03-02 19:56:29 +0900

    1

--- Assets/Scripts/Misc/Crosshair.cs
+++ Assets/Scripts/Misc/Crosshair.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Crosshair : MonoBehaviour
+public class Crosshair : MonoBehaviour, IPointerMoveHandler
 {
+    private Vector3 _currentMousePosition;
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        print(1);
+    }
+
+    private void Start()
+    {
+        _currentMousePosition = Input.mousePosition;
+    }
+
     private void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
-        transform.position = mousePosition;
+        if (_currentMousePosition != Input.mousePosition)
+        {
+            print("mouse is moving");
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+            _currentMousePosition = Input.mousePosition;
+        }
+        else
+        {
+            print("mouse is stopped");
+        }
     }
 }
